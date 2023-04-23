17c0b2c7dae2b94defa7714701017cf5a44f5123
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-04-23 17:37:26 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,50 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
+import { CoursesDetailComponent } from './courses-detail/courses-detail.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    CoursesComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ContactCreateComponent,
+    ContactDetailComponent,
+    CoursesDetailComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/courses-detail/courses-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-detail',
+  templateUrl: './courses-detail.component.html',
+  styleUrls: ['./courses-detail.component.css']
+})
+export class CoursesDetailComponent {
+
+}
